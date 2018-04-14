56891ce390041ea1a7b8840d2f031adceca1ea54
Nathan Glover <nathan@glovers.id.au>
2018-04-14 19:49:29 +0800

    L04_S09 Complete

---
+++ L04_S09/lib/ui/cat_details/details_body.dart
@@ -0,0 +1,13 @@
+import 'package:catbox/models/cat.dart';
+import 'package:flutter/material.dart';
+
+class CatDetailBody extends StatelessWidget {
+  final Cat cat;
+
+  CatDetailBody(this.cat);
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}

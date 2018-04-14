a9dc81a3b21010767d8affe583ccfec72bb2d9a6
Nathan Glover <nathan@glovers.id.au>
2018-04-14 18:56:27 +0800

    Skeleton for L04_S08

---
+++ L04_S08/lib/ui/cat_details/details_page.dart
@@ -0,0 +1,23 @@
+import 'package:catbox/models/cat.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+class CatDetailsPage extends StatefulWidget {
+  final Cat cat;
+  final Object avatarTag;
+
+  CatDetailsPage(
+    this.cat, {
+    @required this.avatarTag,
+  });
+
+  @override
+  _CatDetailsPageState createState() => new _CatDetailsPageState();
+}
+
+class _CatDetailsPageState extends State<CatDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}

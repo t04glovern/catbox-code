603b53987b755cbe316d789a1a5479c8e2a2d712
Nathan Glover <nathan@glovers.id.au>
2018-04-16 21:30:31 +0800

    L05_S06 Baseline

---
+++ L05_S06/lib/services/api.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'dart:convert';
+import 'package:catbox/models/cat.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class CatApi {
+  static FirebaseAuth _auth = FirebaseAuth.instance;
+  static GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+  FirebaseUser firebaseUser;
+
+  CatApi(FirebaseUser user) {
+    this.firebaseUser = user;
+  }
+
+  static Future<CatApi> signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final FirebaseUser user = await _auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    return new CatApi(user);
+  }
+
+  static List<Cat> allCatsFromJson(String jsonData) {
+    List<Cat> cats = [];
+    json.decode(jsonData)['cats'].forEach((cat) => cats.add(_fromMap(cat)));
+    return cats;
+  }
+
+  static Cat _fromMap(Map<String, dynamic> map) {
+    return new Cat(
+      externalId: map['id'],
+      name: map['name'],
+      description: map['description'],
+      avatarUrl: map['image_url'],
+      location: map['location'],
+      likeCounter: map['like_counter'],
+      isAdopted: map['adopted'],
+      pictures: new List<String>.from(map['pictures']),
+      cattributes: new List<String>.from(map['cattributes']),
+    );
+  }
+}
---
+++ L05_S06/lib/ui/cat_details/details_body.dart
@@ -0,0 +1,79 @@
+import 'package:catbox/models/cat.dart';
+import 'package:flutter/material.dart';
+
+class CatDetailBody extends StatelessWidget {
+  final Cat cat;
+
+  CatDetailBody(this.cat);
+
+  _createCircleBadge(IconData iconData, Color color) {
+    return new Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: new CircleAvatar(
+        backgroundColor: color,
+        child: new Icon(
+          iconData,
+          color: Colors.white,
+          size: 16.0,
+        ),
+        radius: 16.0,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var textTheme = theme.textTheme;
+
+    var locationInfo = new Row(
+      children: [
+        new Icon(
+          Icons.place,
+          color: Colors.white,
+          size: 16.0,
+        ),
+        new Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: new Text(
+            cat.location,
+            style: textTheme.subhead.copyWith(color: Colors.white),
+          ),
+        ),
+      ],
+    );
+
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        new Text(
+          cat.name,
+          style: textTheme.headline.copyWith(color: Colors.white),
+        ),
+        new Padding(
+          padding: const EdgeInsets.only(top: 4.0),
+          child: locationInfo,
+        ),
+        // Badges
+        new Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: new Text(
+            cat.description,
+            style:
+                textTheme.body1.copyWith(color: Colors.white70, fontSize: 16.0),
+          ),
+        ),
+        new Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: new Row(
+            children: [
+              _createCircleBadge(Icons.share, theme.accentColor),
+              _createCircleBadge(Icons.phone, Colors.white12),
+              _createCircleBadge(Icons.email, Colors.white12),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}

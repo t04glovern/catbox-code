0ee91f5522312de0789246b71ace066e4a5ddc84
Nathan Glover <nathan@glovers.id.au>
2018-04-26 20:22:35 +0800

    L02_S02 Complete

--- L02_S02/lib/main.dart
+++ L02_S02/lib/main.dart
@@ -10,6 +10,12 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter is Cool!'),
+          leading: new Icon(
+            Icons.cake
+          ),
+        ),
         body: new Center(
           child: new Text('Hello World'),
         ),

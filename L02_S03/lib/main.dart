361473bfc95ceea4c0b38936963957dae3ef9e09
Nathan Glover <nathan@glovers.id.au>
2018-04-26 20:55:58 +0800

    L02_S03 Complete

--- L02_S03/lib/main.dart
+++ L02_S03/lib/main.dart
@@ -15,6 +15,23 @@ class MyApp extends StatelessWidget {
           leading: new Icon(
             Icons.cake
           ),
+          actions: <Widget>[
+            new IconButton(
+              icon: new Icon(Icons.sd_card),
+              tooltip: 'Hi!',
+              onPressed: () => {},
+            ),
+            new IconButton(
+              icon: new Icon(Icons.pie_chart),
+              tooltip: 'Wow!',
+              onPressed: () => {},
+            ),
+            new IconButton(
+              icon: new Icon(Icons.photo_album),
+              tooltip: 'Snap!',
+              onPressed: () => {},
+            ),
+          ],
         ),
         body: new Center(
           child: new Text('Hello World'),
